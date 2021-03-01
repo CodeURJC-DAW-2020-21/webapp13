d7f711a5e1a53cbfd5e8c16db75c28c81fa7e13e
Cristian de Gracia <cris.dgrnu@gmail.com>
2021-03-01 12:53:32 +0100

    Search explore and home link

---
+++ backend/src/main/java/es/webapp13/porthub/AppController.java
@@ -0,0 +1,33 @@
+package es.webapp13.porthub;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String indexLink(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/search")
+    public String searchLink(Model model) {
+        model.addAttribute("active_search", true);
+        return "search";
+    }
+
+    @GetMapping("/shop")
+    public String shopLink(Model model) {
+        model.addAttribute("active_shop", true);
+        return "shop";
+    }
+
+    @GetMapping("/templates/premium/index")
+    public String templateLink(Model model) {
+        return "templates/premium/index";
+    }
+
+
+}
