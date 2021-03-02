d67ffbe91248317dce17ae484efc16608a3127c8
Cristian de Gracia <cris.dgrnu@gmail.com>
2021-03-02 10:56:47 +0100

    Links

--- backend/src/main/java/es/webapp13/porthub/controller/AppController.java
+++ backend/src/main/java/es/webapp13/porthub/controller/AppController.java
@@ -18,16 +18,51 @@ public class AppController {
         return "search";
     }
 
+    @GetMapping("/templates/premium/index")
+    public String templateLink(Model model) {
+        return "templates/premium/index";
+    }
+
+    @GetMapping("/templates/premium/portfolioitem")
+    public String templatePortfolioItemLink(Model model) {
+        return "templates/premium/portfolio-item";
+    }
+
     @GetMapping("/shop")
     public String shopLink(Model model) {
         model.addAttribute("active_shop", true);
         return "shop";
     }
 
-    @GetMapping("/templates/premium/index")
-    public String templateLink(Model model) {
-        return "templates/premium/index";
+    @GetMapping("/student-edit-account")
+    public String studentEditAccountLink(Model model) {
+        model.addAttribute("active_main", true);
+        return "student-edit-account";
     }
+
+    @GetMapping("/student-edit-account-notifications")
+    public String studentEditAccountNotificationsLink(Model model) {
+        model.addAttribute("active_notifications", true);
+        return "student-edit-account-notifications";
+    }
+
+    @GetMapping("/student-edit-account-password")
+    public String studentEditAccountPasswordLink(Model model) {
+        model.addAttribute("active_password", true);
+        return "student-edit-account-password";
+    }
+
+    @GetMapping("/student-edit-account-profile")
+    public String studentEditAccountProfileLink(Model model) {
+        model.addAttribute("active_profile", true);
+        return "student-edit-account-profile";
+    }
+
+    @GetMapping("/login")
+    public String loginLink(Model model) {
+        return "login";
+    }
+
 
 
 }
