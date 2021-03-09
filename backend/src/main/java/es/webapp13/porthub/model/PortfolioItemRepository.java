1f14b29bad83abf1497003f636ecfe707d2bfd32
Aitor <aitorortega21@gmail.com>
2021-03-09 09:36:50 +0100

    PortfolioItem Repo added and coded

---
+++ backend/src/main/java/es/webapp13/porthub/model/PortfolioItemRepository.java
@@ -0,0 +1,6 @@
+package es.webapp13.porthub.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PortfolioItemRepository extends JpaRepository<PortfolioItem, Integer> {
+}
