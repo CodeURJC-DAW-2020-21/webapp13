9ed84fcfd9dd80a54551cf3e195cb905a8d7ec67
JMAguado <josemanuel.aguado.g@gmail.com>
2021-03-10 17:14:23 +0100

    Fixed user register

--- backend/src/main/java/es/webapp13/porthub/controller/AppController.java
+++ backend/src/main/java/es/webapp13/porthub/controller/AppController.java
@@ -3,14 +3,11 @@ package es.webapp13.porthub.controller;
 import es.webapp13.porthub.model.PortfolioItem;
 import es.webapp13.porthub.model.User;
 import es.webapp13.porthub.service.UserService;
-import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 
 import java.io.IOException;
@@ -108,7 +105,7 @@ public class AppController {
     }
 
     @PostMapping("/signup-confirmation")
-    public String signupConfirmationLink(Model model, User user, @RequestParam MultipartFile imageFile) throws IOException {
+    public String signupConfirmationLink(Model model, User user) throws IOException {
         userService.createUser(user);
         return "signup-confirmation";
     }
--- backend/src/main/java/es/webapp13/porthub/service/UserService.java
+++ backend/src/main/java/es/webapp13/porthub/service/UserService.java
@@ -30,6 +30,7 @@ public class UserService {
     public void createUser(User user) {
         user.setActiveTemplate(templateRepository.findFirstById(2));
 
+
         userRepository.save(user);
     }
 
