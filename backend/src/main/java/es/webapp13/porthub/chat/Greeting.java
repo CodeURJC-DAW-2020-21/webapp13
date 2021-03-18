ffc5b35ed8e0d50a85ec7c722608c0eff8a87f84
JMAguado <josemanuel.aguado.g@gmail.com>
2021-03-18 16:30:59 +0100

    Basic chat implemented

---
+++ backend/src/main/java/es/webapp13/porthub/chat/Greeting.java
@@ -0,0 +1,18 @@
+package es.webapp13.porthub.chat;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ backend/src/main/java/es/webapp13/porthub/chat/GreetingController.java
@@ -0,0 +1,25 @@
+package es.webapp13.porthub.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+    @GetMapping("/hello")
+    public String chat(){
+        return "hello";
+    }
+
+}
