6f7b0d13d43a197418259af89849b1eea2479e04
aitorortega03 <Aitor.2020>
2021-03-21 17:34:17 +0100

    DataBaseController users example data modified, only need to rewrite the fields of description and freelance

--- backend/src/main/java/es/webapp13/porthub/controller/DataBaseController.java
+++ backend/src/main/java/es/webapp13/porthub/controller/DataBaseController.java
@@ -76,8 +76,8 @@ public class DataBaseController implements CommandLineRunner {
         portfolioItemList.add(portfolioItem6);
         portfolioItemList.add(portfolioItem7);
 
-        User user = new User("id", "name1", "surname", "email@email",passwordEncoder.encode("pass"), "618 99 55 66",
-                "www.web.es", "Madrid", "Grado en Ingeniería Informática", "freelance", "description", "Ingeniero", free,new Date(161598600),portfolioItemList, "USER");
+        User user = new User("educam17", "Eduardo", "Camero", "educam123@gmail.com",passwordEncoder.encode("pass"), "618 99 55 66",
+                "www.urjc.es", "Madrid", "Grado en Ingeniería Informática", "freelance", "Persona trabajadora y constante", "Ingeniero", free,new Date(161588600),portfolioItemList, "USER");
         userRepository.save(user);
         user.getTemplates().add(free);
         activeTemplateService.init(user.getTemplates(), user.getActiveTemplate());
@@ -85,62 +85,62 @@ public class DataBaseController implements CommandLineRunner {
 
 
 
-        userRepository.save(new User("id2", "name2", "surname2", "email2",passwordEncoder.encode("1234"), "phoneNumber2",
-                "website2", "city2", "degree2", "freelance2", "description2", "Fotografo", premium, new Date(161598600),"USER", "ADMIN"));
+        userRepository.save(new User("lmessi10", "Lionel", "Messi", "lionelamessi10@gmail.com",passwordEncoder.encode("1234"), "635 890 173",
+                "www.pinterest.com", "Barcelona", "Técnico de imagen y sonido", "freelance2", "Persona con rápido aprendizaje y adaptación", "Fotografo", premium, new Date(162598600),"USER", "ADMIN"));
 
-        userRepository.save(new User("id23", "name3", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("ppdomi3", "José", "Dominguez", "ppdominguez14@hotmail.com",passwordEncoder.encode("pass"), "phoneNumber",
+                "www.radiomarca.com", "Valencia", "Grado de Ingeniería de Telecomunicaciones", "freelance", "Amplia experiencia laboral trabajando para empresas como Telefonica", "Ingeniero", free,new Date(141598600), "USER"));
 
-        userRepository.save(new User("id3", "name4", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("jdelgado00", "Juan", "Delgado", "jdelgado2020@gmail.com",passwordEncoder.encode("pass"), "624 563 789",
+                "www.twitch.com", "San Sebastián", "Grado de Ingenieria de Caminos", "freelance", "description", "Ingeniero", free,new Date(151598600), "USER"));
 
-        userRepository.save(new User("id4", "name5", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("dperez15", "David", "Perez", "davidperez10@yahoo.com",passwordEncoder.encode("pass"), "623 768 654",
+                "www.youtube.com", "París", "Grado de Diseño de Interiores", "freelance", "description", "Diseñador", free,new Date(163598600), "USER"));
 
-        userRepository.save(new User("id5", "name6", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("csanchez85", "Carlos", "Sanchez", "carlossanchez99@oulook.com",passwordEncoder.encode("pass"), "629 798 657",
+                "www.bitpanda.com", "Valladolid", "Grado de Ingenieria de la Energía", "freelance", "description", "Ingeniero", free,new Date(181598600), "USER"));
 
-        userRepository.save(new User("id6", "name7", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("rogonza987", "Rodrigo", "Gonzalez", "rodrigonzalez7@gmail.com",passwordEncoder.encode("pass"), "604 347 927",
+                "www.telegram.com", "Toledo", "Grado de Ingeniería Medioambiental", "freelance", "", "Ingeniero", free,new Date(167598600), "USER"));
 
-        userRepository.save(new User("id7", "name8", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("anitarom56", "Ana", "Romero", "anaromeroest79@gmail.com",passwordEncoder.encode("pass"), "690 216 237",
+                "www.instagram.com", "Cáceres", "Grado de Ingenieria Electrica", "freelance", "description", "Ingeniero", free,new Date(169598600), "USER"));
 
-        userRepository.save(new User("id8", "name9", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("siiil99", "Silvia", "Ballesteros", "silviaballesteros98@outlook.com",passwordEncoder.encode("pass"), "639 289 038",
+                "www.tuenti.com", "Badajoz", "Grado de Fundamentos de la fotografia", "freelance", "description", "Fotografo", free,new Date(161298600), "USER"));
 
-        userRepository.save(new User("id9", "name10", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("lauradiaz67", "Laura", "Díaz", "lauradiezmad20@telefonica.net",passwordEncoder.encode("pass"), "699 236 546",
+                "www.facebook.com", "Londres", "Grado de Ingeniería del software", "freelance", "description", "Ingeniero", free,new Date(161558600), "USER"));
 
-        userRepository.save(new User("id10", "name11", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free, new Date(161598600),"USER"));
+        userRepository.save(new User("bertacabello25", "Berta", "Cabello", "bertacabello50@gmail.com",passwordEncoder.encode("pass"), "634 847 935",
+                "www.snapchat.com", "Vigo", "Grado de Ingeniería Geologica", "freelance", "description", "Ingeniero", free, new Date(166598600),"USER"));
 
-        userRepository.save(new User("id234", "name12", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("crisheredia10", "Cristina", "Heredia", "crisheredia11@hotmail.com",passwordEncoder.encode("pass"), "619 025 549",
+                "www.elmundo.com", "A Coruña", "Doble grado de Ingenieria Informatica y Software", "freelance", "description", "Ingeniero", free,new Date(111598600), "USER"));
 
-        userRepository.save(new User("id35", "name13", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("leoguti156", "Leopoldo", "Gutierrez", "leogutierrez1996@yahoo.es",passwordEncoder.encode("pass"), "618 289 823",
+                "www.as.com", "Mallorca", "Grado de Administración y Dirección de Empresas", "freelance", "description", "Empresario", free,new Date(121508600), "USER"));
 
-        userRepository.save(new User("id46", "name14", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("sergiosp21", "Sergio", "Perez", "sergioperezsp@gmail.com",passwordEncoder.encode("pass"), "602 978 945 ",
+                "www.linkedn.com", "Ibiza", "Grado de Comunicacion Audiovisual", "freelance", "description", "Fotografia", free,new Date(191598600), "USER"));
 
-        userRepository.save(new User("id56", "name15", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("dionig2", "Dionisio", "Guerrero", "dionielguerrero37@outlook.com",passwordEncoder.encode("pass"), "629 783 292",
+                "www.spotify.com", "Murcia", "Grado de Ingeniera Matematica", "freelance", "description", "Ingeniero", free,new Date(161398600), "USER"));
 
-        userRepository.save(new User("id66", "name16", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("manuarias89", "Manuel", "Arias", "manarias567@gmail.com",passwordEncoder.encode("pass"), "627 472 343",
+                "www.discord.com", "Cartagena", "Doble grado de Ingenieria Informatica y Medicina", "freelance", "description", "Ingeniero", free,new Date(161598200), "USER"));
 
-        userRepository.save(new User("id76", "name17", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("anselagudo74", "Anselmo", "Agudo", "anselagudo3@gmail.com",passwordEncoder.encode("pass"), "610 497 823",
+                "www.rtve.es", "León", "Grado de Ingenieria Aeroespacial", "freelance", "description", "Ingeniero", free,new Date(171593600), "USER"));
 
-        userRepository.save(new User("id88", "name18", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("ivancanosp", "Ivan", "Cano", "ivancanosyp@grimey.es",passwordEncoder.encode("pass"), "632 708 916",
+                "www.whatsapp.com", "Soria", "Doble grado de Administracion y Direccion de Empresas y Derecho", "freelance", "description", "Empresario", free,new Date(161598600), "USER"));
 
-        userRepository.save(new User("id99", "name19", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("costantonio6", "Antonio", "Costa", "tonicosta45@telfonica.net",passwordEncoder.encode("pass"), "602 472 938",
+                "www.twitter.com", "Ciudad Real", "Grado de Economia", "freelance", "description", "Empresario", free,new Date(261598600), "USER"));
 
-        userRepository.save(new User("id108", "name20", "surname", "email",passwordEncoder.encode("pass"), "phoneNumber",
-                "website", "city", "degree", "freelance", "description", "Ingeniero", free,new Date(161598600), "USER"));
+        userRepository.save(new User("a.lopez.76", "Abraham", "Lopez", "abrahamlopez12@gmail.com",passwordEncoder.encode("pass"), "619 749 853",
+                "www.ask.fm", "Sevilla", "Grado de Ingenieria de Algoritmos", "freelance", "description", "Ingeniero", free,new Date(169598600), "USER"));
 
 
     }
