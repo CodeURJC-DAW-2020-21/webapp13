3dcf8e9b05a6a7574f10f73c2b06f2a48bfb17e8
jj-tena <josejustotena0@gmail.com>
2021-04-08 17:06:06 +0200

    Created a DTO for portfolio item

---
+++ backend/src/main/java/es/webapp13/porthub/api/portfolio_item/PortfolioItemDTO.java
@@ -0,0 +1,36 @@
+package es.webapp13.porthub.api.portfolio_item;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+import java.sql.Date;
+
+@Getter
+@Setter
+public class PortfolioItemDTO {
+
+    private long id;
+
+    private String userId;
+
+    private String name;
+
+    private String description;
+
+    private MultipartFile previewImg;
+
+    private MultipartFile image1;
+
+    private MultipartFile image2;
+
+    private MultipartFile image3;
+
+    private String category;
+
+    private String client;
+
+    private String url;
+
+    private Date date;
+}
