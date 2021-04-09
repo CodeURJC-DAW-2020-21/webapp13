aa6a9eb123fbace3f69677d11f7ab0bbcc8cb94a
Cristian de Gracia <cris.dgrnu@gmail.com>
2021-04-09 13:23:37 +0200

    Functional way of uploading photos

--- backend/src/main/java/es/webapp13/porthub/service/PortfolioItemService.java
+++ backend/src/main/java/es/webapp13/porthub/service/PortfolioItemService.java
@@ -1,9 +1,9 @@
 package es.webapp13.porthub.service;
 
-import es.webapp13.porthub.repository.PortfolioItemRepository;
-import es.webapp13.porthub.repository.UserRepository;
 import es.webapp13.porthub.model.PortfolioItem;
 import es.webapp13.porthub.model.User;
+import es.webapp13.porthub.repository.PortfolioItemRepository;
+import es.webapp13.porthub.repository.UserRepository;
 import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Component
@@ -36,7 +35,7 @@ public class PortfolioItemService {
      * @param userId Id of the user
      * @param item   Form with the portfolio item information
      */
-    public void addPortfolioItem(String userId, PortfolioItem item, MultipartFile previewImg, MultipartFile image1, MultipartFile image2, MultipartFile image3) throws IOException {
+    public void create(String userId, PortfolioItem item, MultipartFile previewImg, MultipartFile image1, MultipartFile image2, MultipartFile image3) throws IOException {
         item.setUserId(userId);
         item.setPreviewImg(BlobProxy.generateProxy(previewImg.getInputStream(), previewImg.getSize()));
         item.setImage1(BlobProxy.generateProxy(image1.getInputStream(), image1.getSize()));
@@ -49,246 +48,24 @@ public class PortfolioItemService {
         userRepository.save(user);
     }
 
-    public void addPortfolioItem(String userId, PortfolioItem item) throws IOException {
-        item.setUserId(userId);
-        portfolioItemRepository.save(item);
-
-        User user = userRepository.findById(userId).orElseThrow();
-        user.addPortfolioItem(item);
-        userRepository.save(user);
-    }
-
     /**
-     * Return a portfolio item by a given user id and portfolio item id
+     * Save a portfolio item
      *
-     * @param userId Id of the user
-     * @param itemId Id of the portfolio item
-     * @return Portfolio item
+     * @param portfolioItem A given portfolio item
      */
-    public PortfolioItem getPortfolioItem(String userId, long itemId) {
-        User user = userRepository.findById(userId).orElseThrow();
-        List<PortfolioItem> portfolioItemList = user.getPortfolioItems();
-        for (PortfolioItem p : portfolioItemList) {
-            if (p.getId() == itemId)
-                return p;
-        }
-        throw new NoSuchElementException("No portfolio item associated to this user id");
-    }
-
-    /**
-     * Update a portfolio item fields
-     *
-     * @param item form with the changes to the portfolio item
-     * @param id   id of the portfolio item
-     */
-    public void updatePortfolioItem(PortfolioItem item, long id, MultipartFile previewImg, MultipartFile image1, MultipartFile image2, MultipartFile image3) throws IOException, SQLException {
-        PortfolioItem portfolioItem = portfolioItemRepository.findById(id).orElseThrow();
-        updatePreviewImg(portfolioItem, previewImg);
-        updateImg1(portfolioItem, image1);
-        updateImg2(portfolioItem, image2);
-        updateImg3(portfolioItem, image3);
-        portfolioItem.setName(item.getName());
-        portfolioItem.setCategory(item.getCategory());
-        portfolioItem.setClient(item.getClient());
-        portfolioItem.setDate(item.getDate());
-        portfolioItem.setUrl(item.getUrl());
-        portfolioItem.setDescription(item.getDescription());
+    public void save(PortfolioItem portfolioItem) {
         portfolioItemRepository.save(portfolioItem);
     }
 
     /**
-     * Update a portfolio item without modifying the images
+     * Get a page with 3 portfolio items
      *
-     * @param item
-     * @param id
-     * @throws IOException
-     * @throws SQLException
+     * @param userId   Id of the user
+     * @param pageable Pageable param
+     * @return A page with 3 portfolio items
      */
-    public void updatePortfolioItem(PortfolioItem item, long id) throws IOException, SQLException {
-        PortfolioItem portfolioItem = portfolioItemRepository.findById(id).orElseThrow();
-        portfolioItem.setName(item.getName());
-        portfolioItem.setCategory(item.getCategory());
-        portfolioItem.setClient(item.getClient());
-        portfolioItem.setDate(item.getDate());
-        portfolioItem.setUrl(item.getUrl());
-        portfolioItem.setDescription(item.getDescription());
-        portfolioItemRepository.save(portfolioItem);
-    }
-
-    /**
-     * Set Preview image
-     *
-     * @param item       Portfolio item to set preview image
-     * @param previewImg Portfolio preview image
-     * @throws IOException  Not found input image
-     * @throws SQLException Not found in DB
-     */
-    public void setPreviewImg(PortfolioItem item, MultipartFile previewImg) throws IOException, SQLException {
-        if (!previewImg.isEmpty())
-            item.setPreviewImg(BlobProxy.generateProxy(previewImg.getInputStream(), previewImg.getSize()));
-        else {
-
-            // Maintain the same image loading it before updating the book
-            PortfolioItem dbItem = findById(item.getId()).orElseThrow();
-            if (dbItem.getPreviewImg().length() == 0)
-                item.setPreviewImg(BlobProxy.generateProxy(dbItem.getPreviewImg().getBinaryStream(), dbItem.getPreviewImg().length()));
-        }
-    }
-
-    /**
-     * Update Preview image
-     *
-     * @param item       Portfolio item to update preview image
-     * @param previewImg Portfolio preview image
-     * @throws IOException  Not found input image
-     * @throws SQLException Not found in DB
-     */
-    public void updatePreviewImg(PortfolioItem item, MultipartFile previewImg) throws IOException, SQLException {
-        if (!previewImg.isEmpty())
-            item.setPreviewImg(BlobProxy.generateProxy(previewImg.getInputStream(), previewImg.getSize()));
-        else {
-
-            // Maintain the same image loading it before updating the book
-            PortfolioItem dbItem = findById(item.getId()).orElseThrow();
-            if (dbItem.getPreviewImg().length() == 0)
-                item.setPreviewImg(BlobProxy.generateProxy(dbItem.getPreviewImg().getBinaryStream(), dbItem.getPreviewImg().length()));
-        }
-    }
-
-    /**
-     * Set image 1
-     *
-     * @param item Portfolio item to set image
-     * @param img  Portfolio image
-     * @throws IOException  Not found input image
-     * @throws SQLException Not found in DB
-     */
-    public void setImg1(PortfolioItem item, MultipartFile img) throws IOException, SQLException {
-        if (!img.isEmpty())
-            item.setImage1(BlobProxy.generateProxy(img.getInputStream(), img.getSize()));
-        else {
-
-            // Maintain the same image loading it before updating the book
-            PortfolioItem dbItem = findById(item.getId()).orElseThrow();
-            if (dbItem.getImage1().length() == 0)
-                item.setImage1(BlobProxy.generateProxy(dbItem.getImage1().getBinaryStream(), dbItem.getImage1().length()));
-        }
-    }
-
-    /**
-     * Update image 1
-     *
-     * @param item Portfolio item to update image
-     * @param img  Portfolio image
-     * @throws IOException  Not found input image
-     * @throws SQLException Not found in DB
-     */
-    public void updateImg1(PortfolioItem item, MultipartFile img) throws IOException, SQLException {
-        if (!img.isEmpty())
-            item.setImage1(BlobProxy.generateProxy(img.getInputStream(), img.getSize()));
-        else {
-
-            // Maintain the same image loading it before updating the book
-            PortfolioItem dbItem = findById(item.getId()).orElseThrow();
-            if (dbItem.getImage1().length() == 0)
-                item.setImage1(BlobProxy.generateProxy(dbItem.getImage1().getBinaryStream(), dbItem.getImage1().length()));
-        }
-    }
-
-    /**
-     * Set image 2
-     *
-     * @param item Portfolio item to set image
-     * @param img  Portfolio image
-     * @throws IOException  Not found input image
-     * @throws SQLException Not found in DB
-     */
-    public void setImg2(PortfolioItem item, MultipartFile img) throws IOException, SQLException {
-        if (!img.isEmpty())
-            item.setImage2(BlobProxy.generateProxy(img.getInputStream(), img.getSize()));
-        else {
-
-            // Maintain the same image loading it before updating the book
-            PortfolioItem dbItem = findById(item.getId()).orElseThrow();
-            if (dbItem.getImage2().length() == 0)
-                item.setImage2(BlobProxy.generateProxy(dbItem.getImage2().getBinaryStream(), dbItem.getImage2().length()));
-        }
-    }
-
-    /**
-     * Update image 2
-     *
-     * @param item Portfolio item to update image
-     * @param img  Portfolio image
-     * @throws IOException  Not found input image
-     * @throws SQLException Not found in DB
-     */
-    public void updateImg2(PortfolioItem item, MultipartFile img) throws IOException, SQLException {
-        if (!img.isEmpty())
-            item.setImage2(BlobProxy.generateProxy(img.getInputStream(), img.getSize()));
-        else {
-
-            // Maintain the same image loading it before updating the book
-            PortfolioItem dbItem = findById(item.getId()).orElseThrow();
-            if (dbItem.getImage2().length() == 0)
-                item.setImage2(BlobProxy.generateProxy(dbItem.getImage2().getBinaryStream(), dbItem.getImage2().length()));
-        }
-    }
-
-    /**
-     * Set image 3
-     *
-     * @param item Portfolio item to set image
-     * @param img  Portfolio image
-     * @throws IOException  Not found input image
-     * @throws SQLException Not found in DB
-     */
-    public void setImg3(PortfolioItem item, MultipartFile img) throws IOException, SQLException {
-        if (!img.isEmpty())
-            item.setImage3(BlobProxy.generateProxy(img.getInputStream(), img.getSize()));
-        else {
-
-            // Maintain the same image loading it before updating the book
-            PortfolioItem dbItem = findById(item.getId()).orElseThrow();
-            if (dbItem.getImage3().length() == 0)
-                item.setImage3(BlobProxy.generateProxy(dbItem.getImage3().getBinaryStream(), dbItem.getImage3().length()));
-        }
-    }
-
-    /**
-     * Update image 3
-     *
-     * @param item Portfolio item to update image
-     * @param img  Portfolio image
-     * @throws IOException  Not found input image
-     * @throws SQLException Not found in DB
-     */
-    public void updateImg3(PortfolioItem item, MultipartFile img) throws IOException, SQLException {
-        if (!img.isEmpty())
-            item.setImage3(BlobProxy.generateProxy(img.getInputStream(), img.getSize()));
-        else {
-
-            // Maintain the same image loading it before updating the book
-            PortfolioItem dbItem = findById(item.getId()).orElseThrow();
-            if (dbItem.getImage3().length() == 0)
-                item.setImage3(BlobProxy.generateProxy(dbItem.getImage3().getBinaryStream(), dbItem.getImage3().length()));
-        }
-    }
-
-    /**
-     * Delete a portfolio item by a given userId and itemId
-     *
-     * @param userId id of the user
-     * @param itemId id of the item
-     */
-    public void deletePortfolioItem(String userId, long itemId) {
-        User user = userRepository.findById(userId).orElseThrow();
-        List<PortfolioItem> portfolioItemList = user.getPortfolioItems();
-        PortfolioItem portfolioItem = portfolioItemRepository.findById(itemId).orElseThrow();
-
-        portfolioItemList.remove(portfolioItem);
-        portfolioItemRepository.delete(portfolioItem);
-        userRepository.save(user);
+    public Page<PortfolioItem> findPage(String userId, Pageable pageable) {
+        return portfolioItemRepository.findByUserId(userId, PageRequest.of(pageable.getPageNumber(), 3));
     }
 
     /**
@@ -302,21 +79,71 @@ public class PortfolioItemService {
     }
 
     /**
-     * Get a page with 3 portfolio items
+     * Update a portfolio item fields
      *
-     * @param userId   Id of the user
-     * @param pageable Pageable param
-     * @return A page with 3 portfolio items
+     * @param item form with the changes to the portfolio item
+     * @param id   id of the portfolio item
      */
-    public Page<PortfolioItem> findPortfolioItems(String userId, Pageable pageable) {
-        return portfolioItemRepository.findByUserId(userId, PageRequest.of(pageable.getPageNumber(), 3));
+    public void update(PortfolioItem item, long id, MultipartFile previewImg, MultipartFile image1, MultipartFile image2, MultipartFile image3) throws IOException, SQLException {
+        PortfolioItem portfolioItem = portfolioItemRepository.findById(id).orElseThrow();
+
+        setImage setPreviewImg = (p, i) -> p.setPreviewImg(BlobProxy.generateProxy(i.getInputStream(), i.getSize()));
+        setImage setImg1 = (p, i) -> p.setImage1(BlobProxy.generateProxy(i.getInputStream(), i.getSize()));
+        setImage setImg2 = (p, i) -> p.setImage2(BlobProxy.generateProxy(i.getInputStream(), i.getSize()));
+        setImage setImg3 = (p, i) -> p.setImage3(BlobProxy.generateProxy(i.getInputStream(), i.getSize()));
+
+        getImage getPreviewImg = (p) -> p.getPreviewImg().length() == 0;
+        getImage getImg1 = (p) -> p.getImage1().length() == 0;
+        getImage getImg2 = (p) -> p.getImage2().length() == 0;
+        getImage getImg3 = (p) -> p.getImage3().length() == 0;
+
+        UpdateImage(portfolioItem, previewImg, setPreviewImg, getPreviewImg);
+        UpdateImage(portfolioItem, image1, setImg1, getImg1);
+        UpdateImage(portfolioItem, image2, setImg2, getImg2);
+        UpdateImage(portfolioItem, image3, setImg3, getImg3);
+
+        portfolioItem.setName(item.getName());
+        portfolioItem.setCategory(item.getCategory());
+        portfolioItem.setClient(item.getClient());
+        portfolioItem.setDate(item.getDate());
+        portfolioItem.setUrl(item.getUrl());
+        portfolioItem.setDescription(item.getDescription());
+        portfolioItemRepository.save(portfolioItem);
     }
 
     /**
-     * Save a portfolio item
-     * @param portfolioItem A given portfolio item
+     * Generic method to update portfolio items images
+     *
+     * @param item   Portfolio item to be updated
+     * @param img    Image to be updated
+     * @param setImg Lambda expression to set image
+     * @param getImg Lambda expression to get image
+     * @throws SQLException Image not found in database
+     * @throws IOException  No image at input
      */
-    public void save(PortfolioItem portfolioItem) {
-        portfolioItemRepository.save(portfolioItem);
+    private void UpdateImage(PortfolioItem item, MultipartFile img, setImage setImg, getImage getImg) throws SQLException, IOException {
+        if (!img.isEmpty())
+            setImg.run(item, img);
+        else {
+            PortfolioItem dbItem = findById(item.getId()).orElseThrow();
+            if (getImg.run(dbItem))
+                item.setPreviewImg(BlobProxy.generateProxy(dbItem.getPreviewImg().getBinaryStream(), dbItem.getPreviewImg().length()));
+        }
+    }
+
+    /**
+     * Delete a portfolio item by a given userId and itemId
+     *
+     * @param userId id of the user
+     * @param itemId id of the item
+     */
+    public void delete(String userId, long itemId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        List<PortfolioItem> portfolioItemList = user.getPortfolioItems();
+        PortfolioItem portfolioItem = portfolioItemRepository.findById(itemId).orElseThrow();
+
+        portfolioItemList.remove(portfolioItem);
+        portfolioItemRepository.delete(portfolioItem);
+        userRepository.save(user);
     }
 }
