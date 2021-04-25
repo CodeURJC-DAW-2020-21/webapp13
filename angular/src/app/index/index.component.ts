cc59b4531c3d5b1754f32f55a86ccd3c5d017067
Cristian de Gracia <cris.dgrnu@gmail.com>
2021-04-25 17:18:08 +0200

    Routing

---
+++ angular/src/app/index/index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
