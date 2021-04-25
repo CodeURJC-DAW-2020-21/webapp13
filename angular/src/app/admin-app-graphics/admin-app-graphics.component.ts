78507ffe986a383456d913421c2ef6523898df00
aitorortega03 <Aitor.2020>
2021-04-25 19:00:27 +0200

    Done the admin components

---
+++ angular/src/app/admin-app-graphics/admin-app-graphics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-app-graphics',
+  templateUrl: './admin-app-graphics.component.html',
+  styleUrls: ['./admin-app-graphics.component.css']
+})
+export class AdminAppGraphicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { IndexComponent } from './index/index.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminTemplatesListComponent } from './admin-templates-list/admin-templates-list.component';
+import { AdminAppGraphicsComponent } from './admin-app-graphics/admin-app-graphics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    IndexComponent
+    IndexComponent,
+    AdminComponent,
+    AdminTemplatesListComponent,
+    AdminAppGraphicsComponent
   ],
   imports: [
     BrowserModule,
