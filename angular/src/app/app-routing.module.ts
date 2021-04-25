5ee6ff2b15acd48b09081f2129d299bd2ceade47
Cristian de Gracia <cris.dgrnu@gmail.com>
2021-04-25 19:32:25 +0200

    Register route

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component'
 import { SearchComponent } from './search/search.component'
 import { SettingsEditAccountComponent } from './settings-edit-account/settings-edit-account.component'
+import { RegisterComponent } from './register/register.component'
 
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'search', component: SearchComponent },
   { path: 'settings-edit-account', component: SettingsEditAccountComponent},
+  { path: 'register', component: RegisterComponent}
 
 ];
 
