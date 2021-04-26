b5efb801a56773271efb354a64e2903c4eecaad7
Cristian de Gracia <cris.dgrnu@gmail.com>
2021-04-26 18:14:38 +0200

    Register users

--- angular/src/app/components/register/register.component.ts
+++ angular/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'register',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
+  register(id:string,email:string,name:string,surname:string,password:string,phoneNumber:string,bornDate:string,city:string,freelance:string,category:string,degree:string,website:string,description:string){
+    this.userService.postUser(id,email,name,surname,password,phoneNumber,bornDate,city,freelance,category,degree,website,description).subscribe(
+      user => {
+        console.log(user)
+      },
+      error => console.log("error")
+    )
+  }
 }
--- angular/src/app/services/user.service.ts
+++ angular/src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map } from 'rxjs/operators'
 import { Observable } from "rxjs";
+import { User } from "../models/user.model";
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
@@ -29,6 +30,12 @@ export class UserService {
     getTotalElements(url:string): Observable<number> {
         return this.httpClient.get(url).pipe(
             map(response => this.extractTotalElements(response as number))
+        )
+    }
+
+    postUser(id:string,email:string,name:string,surname:string,password:string,phoneNumber:string,bornDate:string,city:string,freelance:string,category:string,degree:string,website:string,description:string):Observable<User> {
+        return this.httpClient.post("/api/users/",{id,name,surname,password,phoneNumber,bornDate,city,freelance,category,degree,website,description}).pipe(
+            map(response => this.extractUserProfilePhoto(response as User))
         )
     }
 
