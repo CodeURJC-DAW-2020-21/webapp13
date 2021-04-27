529ef0b55969d5bcb09eb783a962b32fbd9eab5d
jj-tena <josejustotena0@gmail.com>
2021-04-27 14:05:42 +0200

    Settings edit account page shows user information

--- angular/src/app/components/settings-edit-account/settings-edit-account.component.ts
+++ angular/src/app/components/settings-edit-account/settings-edit-account.component.ts
@@ -1,4 +1,6 @@
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-settings-edit-account',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsEditAccountComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUser("id",0)
+  }
+
+  getUser(userId: string, page:number){
+    console.log(userId)
+    this.userService.getUser(userId).subscribe(
+      item => this.user = new User(item),
+      error => console.log("error")
+    )
+    console.log(this.user.content.name)
   }
 
 }
