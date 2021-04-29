485b6f1d239422442524e275a0f6dfdf74103aa2
jj-tena <josejustotena0@gmail.com>
2021-04-29 14:03:02 +0200

    Implemented correct visualization of templates in the shop

--- angular/src/app/components/shop/shop.component.ts
+++ angular/src/app/components/shop/shop.component.ts
@@ -1,4 +1,9 @@
+import { TemplateService } from './../../services/template.service';
+import { LoginService } from './../../services/login.service';
+import { UserService } from './../../services/user.service';
+import { Template } from './../../models/template.model';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-shop',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UserService, private loginService:LoginService, private templateService:TemplateService) { }
+
+  user: User
+
+  purchasedTemplates: Template[] = []
+
+  notPurchasedTemplates: Template[] = []
+
+  recommendedTemplate: Template
 
   ngOnInit(): void {
+
+    this.user = this.loginService.refreshUser()
+
+    if(this.user != undefined){
+      this.userService.getUserTemplates(this.user.content.id).subscribe(
+        templates => {
+          templates.map(template => this.purchasedTemplates.push(template))
+          console.log(this.purchasedTemplates)
+        },
+        error => console.log("error")
+      )
+    }
+
+    this.templateService.getTemplates().subscribe(
+      templates => {
+        templates.map(template => this.notPurchasedTemplates.push(template))
+        console.log(this.notPurchasedTemplates)
+      },
+      error => console.log("error")
+    )
+
   }
+
+  isPurchased(template:Template):boolean {
+  return this.purchasedTemplates.some(function(arrVal) {
+    return template.id === arrVal.id;
+    });
+  }
+
+
 
 }
