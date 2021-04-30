b29ee3354e1d4d932ca5ca1813d5fba9609d3498
Cristian de Gracia <cris.dgrnu@gmail.com>
2021-04-30 14:46:49 +0200

    Index scripts fix

--- angular/src/app/components/index/index.component.ts
+++ angular/src/app/components/index/index.component.ts
@@ -5,45 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
+
 export class IndexComponent implements OnInit {
 
-
-  scripts = [
-    "assets/vendor/popper.min.js",
-    "assets/vendor/jquery.min.js",
-    "assets/vendor/bootstrap.min.js",
-    "assets/vendor/perfect-scrollbar.min.js",
-    "assets/vendor/dom-factory.js",
-    "assets/vendor/material-design-kit.js",
-    "assets/vendor/fix-footer.js",
-    "assets/js/app.js",
-    "assets/js/custom/text-scramble.js"
-  ]
+  
 
   ngOnInit(): void {
   }
 
 
-  loadAPI: Promise<any>;
+
 
   constructor() {
-    this.loadAPI = new Promise((resolve) => {
-      this.loadScript();
-      resolve(true);
-    });
+    
+    
   }
 
-  public loadScript() {
-    this.scripts.forEach(script => {
-      console.log(`Loading script => ${script}`);
-      let node = document.createElement("script");
-      node.src = script
-      node.type = "text/javascript";
-      node.async = true;
-      node.charset = "utf-8";
-      document.getElementsByTagName("head")[0].appendChild(node);
-    });
-  }
+  
 
 
 }
+
