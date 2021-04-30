8ed0871faf322150a05761d7dc0ed021e133c365
jj-tena <josejustotena0@gmail.com>
2021-04-30 18:32:13 +0200

    Update

--- angular/src/app/components/premium-template/premium-template.component.ts
+++ angular/src/app/components/premium-template/premium-template.component.ts
@@ -1,4 +1,7 @@
+import { PortfolioitemService } from './../../services/portfolioitem.service';
+import { LoginService } from './../../services/login.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-premium-template',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PremiumTemplateComponent implements OnInit {
 
-  constructor() { }
+  user: User
+
+  constructor(private loginService:LoginService, private portfolioItemService: PortfolioitemService) { }
 
   ngOnInit(): void {
+    this.user = this.loginService.currentUser()
+    console.log(this.user)
   }
 
 }
